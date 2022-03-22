0d05b67014ffc0ec89f2585702995077c15d368c
Lee Harrison <ralekdev@gmail.com>
2022-03-21 20:47:29 -0700

    Remove unused isValid field

--- src/main/java/com/chaosbuffalo/mknpc/quest/data/objective/UUIDInstanceData.java
+++ src/main/java/com/chaosbuffalo/mknpc/quest/data/objective/UUIDInstanceData.java
@@ -8,16 +8,13 @@ import java.util.UUID;
 public class UUIDInstanceData extends ObjectiveInstanceData {
 
     private UUID uuid;
-    private boolean isValid;
 
     public UUIDInstanceData() {
         uuid = Util.DUMMY_UUID;
-        isValid = false;
     }
 
     public UUIDInstanceData(UUID uuid) {
         this.uuid = uuid;
-        isValid = true;
     }
 
     public UUID getUUID() {
@@ -28,13 +25,11 @@ public class UUIDInstanceData extends ObjectiveInstanceData {
     public CompoundNBT serializeNBT() {
         CompoundNBT tag = new CompoundNBT();
         tag.putUniqueId("id", uuid);
-        tag.putBoolean("isValid", isValid);
         return tag;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
         uuid = nbt.getUniqueId("id");
-        isValid = nbt.getBoolean("isValid");
     }
 }
